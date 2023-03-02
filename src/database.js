00d42c15f33b8ad80473b6b0300dc90826b40af0
Caio Jardim <caiojardinho@gmail.com>
2023-03-02 19:32:10 -0300

    Transform database in to a Class

--- src/database.js
+++ src/database.js
@@ -1,13 +1,20 @@
-let database = {
-  tasks: [
-    {
-      id: 1,
-      title: 'Code Aplication',
-      description: 'Code a Backend task aplication',
-      completed_at: null,
-      created_at: new Date.now(),
-      updated_at: new Date.now()
-    }
-  ]
+class Database {
+  #database = {
+    tasks: [
+      {
+        id: 1,
+        title: 'Code Aplication',
+        description: 'Code a Backend task aplication',
+        completed_at: null,
+        created_at: new Date.now(),
+        updated_at: new Date.now()
+      }
+    ]
+  }
+
+  select() {
+    return tasks
+  }
+  
 }
 
