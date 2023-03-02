98f4c7dc61441dc074f6b602fd51e66f4dcc3a7a
Caio Jardim <caiojardinho@gmail.com>
2023-03-02 19:16:00 -0300

    Define database modeling

---
+++ src/database.js
@@ -0,0 +1,13 @@
+let database = {
+  tasks: [
+    {
+      id: 1,
+      title: 'Code Aplication',
+      description: 'Code a Backend task aplication',
+      completed_at: null,
+      created_at: new Date.now(),
+      updated_at: new Date.now()
+    }
+  ]
+}
+
